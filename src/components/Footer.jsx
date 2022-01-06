af65c1941521947ba8d2770f6220e81d519799ff
radhad-ux <radhad83@gmail.com>
2022-01-07 00:53:55 +0100

    added itemcreator, form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,23 +5,6 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <div className="footer">
-      <div className="icons">
-        <a href="mailto:radhad83@gmail.com" target="blank">
-          <i class="fas fa-envelope"></i>
-        </a>
-        <a
-          href="https://github.com/radhad-ux/eika-shoppinglistapp"
-          target="blank"
-        >
-          <i class="fab fa-fw fa-github"></i>
-        </a>
-        <a href="https://www.linkedin.com/in/radhadoreswamy/" target="blank">
-          <i class="fab fa-fw fa-linkedin-in"></i>
-        </a>
-        <a href="https://instagram/radha/" target="blank">
-          <i class="fab fa-instagram-square"></i>
-        </a>
-      </div>
       <span>©{currentYear} EIKA-Furniture Shop: All rights reserved</span>
     </div>
   );
---
+++ src/screens/WelcomeScreen.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import AddItemCreator from "../components/AddItemCreator";
+import hero from "../img/hero.jpg";
+import "../styles/welcome.css";
+
+export default function WelcomeScreen() {
+  const [itemList, setItemList] = useState([]);
+  return (
+    <div className="welcome">
+      <img src={hero} alt="hero-img" />
+      <div className="welcome-msg">
+        <h2>Welcome to EIKA!</h2>
+        <div className="description">
+          Keep track of your shopping list with the app.Get started by adding a
+          item to your list. You can then sort your shopping list by name or
+          price and mark items as acquired.
+          <br />
+          <strong>Thank you for shopping with us ...</strong>
+        </div>
+      </div>
+      <AddItemCreator itemList={itemList} setItemList={setItemList} />
+    </div>
+  );
+}
