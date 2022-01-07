4ef26c615cc2a5525f0d6288f0ca49f7b15c2be0
radhad-ux <radhad83@gmail.com>
2022-01-07 14:18:37 +0100

    sorting functionality implemented

---
+++ src/components/Sorter.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export default function Sorter({ itemList, setItemList }) {
+  function sortByName() {
+    const sorted = itemList.sort((a, b) => a.name.localCompare(b.name));
+
+    setItemList([...sorted]);
+    localStorage.setItem("itemList", JSON.stringify(itemList));
+  }
+  function sortByPrice() {
+    const sorted = itemList.sort((a, b) => a.price - b.price);
+
+    setItemList([...sorted]);
+    localStorage.setItem("itemList", JSON.stringify(itemList));
+  }
+  return (
+    <div className="sorter">
+      <label>Sort by</label>
+      <button className="btn-sorter" onClick={sortByName}>
+        Name
+      </button>
+      <button className="btn-sorter" onClick={sortByPrice}>
+        Price
+      </button>
+    </div>
+  );
+}
--- src/screens/ListScreen.jsx
+++ src/screens/ListScreen.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export default function ListScreen() {
-  return <div></div>;
+  return (
+    <div className="listscreen">
+      <h1>Your Shopping list</h1>
+    </div>
+  );
 }
